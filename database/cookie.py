9dcb47ceb72831f57ac555fca84d9b065faf538d
FloatingComet62 <shubh2007rai@gmail.com>
2024-09-26 19:20:20 +0530

    I want a cookie

--- database/cookie.py
+++ database/cookie.py
@@ -1,6 +1,12 @@
+from firebase_admin.auth import verify_id_token
+
 class Cookie:
     uid: str
     email: str
     name: str
 
-    def __init__(self, token: str): pass
+    def __init__(self, token: str):
+        decoded = verify_id_token(token)
+        self.uid = decoded['uid']
+        self.email = decoded['email']
+        self.name = decoded['name']
